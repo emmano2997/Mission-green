fd3a6d6d26fd5ae2d87ae63c2b562c34b45ec99b
Emmanuel A. C. Santos <emmanuel.cavalcanti@nave.org.br>
2021-06-10 10:09:40 -0300

    Atualizações de move e menu controler

--- Mission green/Assets/Script/PlayerController.cs
+++ Mission green/Assets/Script/PlayerController.cs
@@ -1,45 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-[RequireComponent(typeof(Rigidbody))]
 public class PlayerController : MonoBehaviour
 {
-    
-    [SerializeField]CharacterController controller; //controle player;
-    [SerializeField] float speed = 5f;  //VelocidadePlayer;
-    [SerializeField] public float VelocidadeDeRotação = 5.0f; //rotação de CAM
-    public float Sensibilidade = 3.0f; // sensibilidade da CAM
-    Rigidbody rb;//Rigidbody
-    public Transform playerTransform;
-    public Vector3 direction;
+public float mouseSensitivity = 100f;
+    public Transform playerBody;
+    private float xRotation = 0f;
 
-    /*private RaycastHist hit;
-    private Ray ray;
-    public LayerMask layer;
-    private Vector3 currentLookTarget = Vector3.zero;
-    private float distance = 30f; */
-    void Update()
-    {
-        rb = GetComponent<Rigidbody>(); //Instanciamento do Rigidbody;
-        
-        //move CAM
-        float rotatehoraziontal = Input.GetAxis("Mouse X");
-        float rotatevertical = Input.GetAxis("Mouse Y");
-        //ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        //Debug.DrawRay(ray.origin,ray.direction * distance, color.blue);
-        Camera.main.transform.RotateAround(transform.position, Vector3.up, rotatehoraziontal * Sensibilidade);
-        Camera.main.transform.RotateAround(transform.position, -Camera.main.transform.right, rotatevertical * Sensibilidade);
-        
-        //move player
-        float horizontal = Input.GetAxisRaw("Horizontal"); 
-        float vertical = Input.GetAxisRaw("Vertical");     
-        
-        Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized; //Mover para horizontal, vertical e trava o Eixo Z;
-        controller.Move(direction * speed * Time.deltaTime); // Velocidade e direção 
-        if (direction.magnitude >= 0.1) //controle da velocidade do player;
-        {
-            controller.Move(direction * speed * Time.deltaTime);
-        }
+    void Start() {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+    void Update() {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
 
-    } 
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+
+        playerBody.Rotate(Vector3.up * mouseX);
+    }
 }
